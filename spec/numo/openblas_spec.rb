0e2a03ef25bc920dee7977bb5facb6f571384f9b
yoshoku <yoshoku@outlook.com>
2020-07-19 00:59:39 +0900

    :sparkles: Add some specs

--- spec/numo/openblas_spec.rb
+++ spec/numo/openblas_spec.rb
@@ -1,7 +1,18 @@
 # frozen-string-literal: true
 
 RSpec.describe Numo::OpenBLAS do
-  it 'has a version number' do
-    expect(Numo::OpenBLAS::VERSION).not_to be nil
+  it 'has a library path' do
+    expect(Numo::Linalg::Loader.libs).not_to be_nil
+    expect(Numo::Linalg::Loader.libs.size).to eq(1)
+    expect(Numo::Linalg::Loader.libs.first).to include('libopenblas')
+  end
+
+  it 'performs linear algebra computation' do
+    a = Numo::DFloat.new(2, 4).rand
+    c = a.dot(a.transpose)
+    evl, evc = Numo::Linalg.eigh(c)
+    d = evc.dot(evl.diag).dot(evc.transpose)
+    err = Math.sqrt(((c - d)**2).sum)
+    expect(err).to be <= 1e-10
   end
 end
