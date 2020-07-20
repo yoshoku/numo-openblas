a79f61f45b18d10461dc03e2307bbe210529d079
yoshoku <yoshoku@outlook.com>
2020-07-20 23:55:07 +0900

    :art: Move constant values to OpenBLAS module

--- lib/numo/openblas/version.rb
+++ lib/numo/openblas/version.rb
@@ -8,5 +8,11 @@ module Numo
 
     # The version OpenBLAS that Numo::OpenBLAS build and use.
     OPENBLAS_VERSION = '0.3.10'
+
+    # The URI of OpenBLAS that Numo::OpenBLAS installed.
+    OPENBLAS_URI = "https://github.com/xianyi/OpenBLAS/archive/v#{OPENBLAS_VERSION}.tar.gz"
+
+    # The install directory of OpenBLAS.
+    INSTALL_DIR = File.expand_path(__dir__ + '/../../../vendor')
   end
 end
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,62 +2,56 @@
 
 require 'rubygems'
 require 'rubygems/package'
+
 require 'etc'
 require 'fileutils'
 require 'open-uri'
 require 'open3'
 
-Gem.post_install do
-  OPENBLAS_VERSION = '0.3.10'
-  OPENBLAS_URI = "https://github.com/xianyi/OpenBLAS/archive/v#{OPENBLAS_VERSION}.tar.gz"
-  OPENBLAS_VENDOR_DIR = File.expand_path(__dir__ + '/../vendor')
-  OPENBLAS_TGZ = "#{OPENBLAS_VENDOR_DIR}/tmp/openblas.tgz"
-  OPENBLAS_LOG = "#{OPENBLAS_VENDOR_DIR}/tmp/openblas.log"
+require 'numo/openblas/version'
 
-  unless File.exist?("#{OPENBLAS_VENDOR_DIR}/installed_#{OPENBLAS_VERSION}")
-    puts "Downloading OpenBLAS #{OPENBLAS_VERSION}."
-    URI.open(OPENBLAS_URI) { |rf| File.open(OPENBLAS_TGZ, 'wb') { |sf| sf.write(rf.read) } }
+Gem.post_install do
+  unless File.exist?("#{Numo::OpenBLAS::INSTALL_DIR}/installed_#{Numo::OpenBLAS::OPENBLAS_VERSION}")
+    puts "Downloading OpenBLAS #{Numo::OpenBLAS::OPENBLAS_VERSION}."
+    URI.open(Numo::OpenBLAS::OPENBLAS_URI) do |rf|
+      File.open("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.tgz", 'wb') { |sf| sf.write(rf.read) }
+    end
 
     puts 'Unpacking OpenBLAS tar.gz file.'
-    Gem::Package::TarReader.new(Zlib::GzipReader.open(OPENBLAS_TGZ)) do |tar|
+    Gem::Package::TarReader.new(Zlib::GzipReader.open("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.tgz")) do |tar|
       tar.each do |entry|
         next unless entry.file?
 
-        filename = "#{OPENBLAS_VENDOR_DIR}/tmp/#{entry.full_name}"
+        filename = "#{Numo::OpenBLAS::INSTALL_DIR}/tmp/#{entry.full_name}"
         next if filename == File.dirname(filename)
 
-        FileUtils.mkdir_p("#{OPENBLAS_VENDOR_DIR}/tmp/#{File.dirname(entry.full_name)}")
+        FileUtils.mkdir_p("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/#{File.dirname(entry.full_name)}")
         File.open(filename, 'wb') { |f| f.write(entry.read) }
         File.chmod(entry.header.mode, filename)
       end
     end
 
-    Dir.chdir("#{OPENBLAS_VENDOR_DIR}/tmp/OpenBLAS-#{OPENBLAS_VERSION}") do
+    Dir.chdir("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/OpenBLAS-#{Numo::OpenBLAS::OPENBLAS_VERSION}") do
       puts 'Building OpenBLAS. This could take a while...'
       mkstdout, _mkstderr, mkstatus = Open3.capture3("make -j#{Etc.nprocessors}")
-      File.open(OPENBLAS_LOG, 'w') { |f| f.puts(mkstdout) }
+      File.open("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.log", 'w') { |f| f.puts(mkstdout) }
       unless mkstatus.success?
         puts 'Failed to build OpenBLAS.'
         puts 'Check the make-openblas.log file for more details:'
-        puts OPENBLAS_LOG
+        puts "#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.log"
         break
       end
+
       puts 'Installing OpenBLAS.'
-      insstdout, _insstderr, insstatus = Open3.capture3("make install PREFIX=#{OPENBLAS_VENDOR_DIR}")
-      File.open(OPENBLAS_LOG, 'a') { |f| f.puts(insstdout) }
+      insstdout, _insstderr, insstatus = Open3.capture3("make install PREFIX=#{Numo::OpenBLAS::INSTALL_DIR}")
+      File.open("#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.log", 'a') { |f| f.puts(insstdout) }
       unless insstatus.success?
         puts 'Failed to install OpenBLAS.'
         puts 'Check the make-openblas.log file for more details:'
-        puts OPENBLAS_LOG
+        puts "#{Numo::OpenBLAS::INSTALL_DIR}/tmp/openblas.log"
       end
+
+      FileUtils.touch("#{Numo::OpenBLAS::INSTALL_DIR}/installed_#{Numo::OpenBLAS::OPENBLAS_VERSION}")
     end
-
-    FileUtils.touch("#{OPENBLAS_VENDOR_DIR}/installed_#{OPENBLAS_VERSION}")
   end
-
-  Object.send(:remove_const, :OPENBLAS_VERSION)
-  Object.send(:remove_const, :OPENBLAS_URI)
-  Object.send(:remove_const, :OPENBLAS_VENDOR_DIR)
-  Object.send(:remove_const, :OPENBLAS_TGZ)
-  Object.send(:remove_const, :OPENBLAS_LOG)
 end
