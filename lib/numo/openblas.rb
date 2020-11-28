8bbecf98a707d2fd582e0fe1843c721e4c153b36
yoshoku <yoshoku@outlook.com>
2020-11-28 11:01:55 +0900

    :art: Simplify code for loading of OpenBLAS library

--- lib/numo/openblas.rb
+++ lib/numo/openblas.rb
@@ -5,27 +5,4 @@ require 'numo/linalg/linalg'
 require 'numo/openblas/version'
 require 'numo/openblas/openblas'
 
-module Numo
-  module Linalg
-    module Loader
-      module_function
-
-      def load_openblas
-        dlext = case RbConfig::CONFIG['host_os']
-                when /mswin|msys|mingw|cygwin/
-                  'dll'
-                when /darwin|mac os/
-                  'dylib'
-                else
-                  'so'
-                end
-        openblas_path = File.expand_path(__dir__ + "/../../vendor/lib/libopenblas.#{dlext}")
-        Numo::Linalg::Blas.dlopen(openblas_path)
-        Numo::Linalg::Lapack.dlopen(openblas_path)
-        @@libs = [openblas_path]
-      end
-    end
-  end
-end
-
-Numo::Linalg::Loader.load_openblas
+Numo::Linalg::Loader.load_openblas(File.expand_path("#{__dir__}/../../vendor/lib/"))
